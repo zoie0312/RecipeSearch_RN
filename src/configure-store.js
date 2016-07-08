191377d83db5008174f24e39c88b57cc0516314b
zoie0312 <greenmoonshisu@gmail.com>
2016-07-08 14:07:30 +0800

    Add redux devtools for debugging

--- src/configure-store.js
+++ src/configure-store.js
@@ -1,11 +1,19 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import rootReducer from './reducers/index'
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
+import devtools from 'remote-redux-devtools';
 
 const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore);
 
 export default function configureStore(initialState) {
-    const store = createStoreWithMiddleware(rootReducer, initialState);
+    const enhancer = compose(
+        applyMiddleware(thunk),
+        devtools()
+    );
+    return createStore(rootReducer, initialState, enhancer);
     
-    return store;
+    //const store = createStoreWithMiddleware(rootReducer, initialState);
+    //return store
+    
 }
